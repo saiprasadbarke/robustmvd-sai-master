1a20722088cd1daae4aff694773774f3519db927
saiprasadbarke <barke.saiprasad1992@gmail.com>
2023-08-28 20:38:26 +0200

    add mvsnet batch augmentations

--- rmvd/data/__init__.py
+++ rmvd/data/__init__.py
@@ -1,6 +1,22 @@
 from .factory import create_dataset, create_compound_dataset
-from .registry import register_dataset, list_base_datasets, list_dataset_types, list_splits, list_datasets, has_dataset
-from .registry import register_augmentation, list_augmentations, has_augmentation, create_augmentation, register_batch_augmentation, list_batch_augmentations, has_batch_augmentation, create_batch_augmentation
+from .registry import (
+    register_dataset,
+    list_base_datasets,
+    list_dataset_types,
+    list_splits,
+    list_datasets,
+    has_dataset,
+)
+from .registry import (
+    register_augmentation,
+    list_augmentations,
+    has_augmentation,
+    create_augmentation,
+    register_batch_augmentation,
+    list_batch_augmentations,
+    has_batch_augmentation,
+    create_batch_augmentation,
+)
 
 # import all datasets; this triggers the registration of the datasets in the registry
 from .eth3d import ETH3DTrainRobustMVD
@@ -13,5 +29,12 @@ from .staticthings3d import StaticThings3DSeq4Train
 from .blendedmvs import BlendedMVSSeq4TrainSmall
 
 # import all augmentations; this triggers the registration of the augmentations in the registry
-from .augmentations import robust_mvd_augmentations_staticthings3d, robust_mvd_augmentations_blendedmvs, supervised_monodepth2_augmentations, mvsnet_augmentations, dpt_augmentations
+from .augmentations import (
+    robust_mvd_augmentations_staticthings3d,
+    robust_mvd_augmentations_blendedmvs,
+    supervised_monodepth2_augmentations,
+    mvsnet_augmentations,
+    dpt_augmentations,
+)
 from .batch_augmentations import robust_mvd_batch_augmentations
+from .batch_augmentations import mvsnet_batch_augmentations
--- rmvd/data/batch_augmentations.py
+++ rmvd/data/batch_augmentations.py
@@ -6,8 +6,18 @@ from .batch_transforms import Scale3DEqualizedBatch, MaskDepthByMinMax
 @register_batch_augmentation
 def robust_mvd_batch_augmentations(**kwargs):
     transforms = [
-        Scale3DEqualizedBatch(p=1, min_depth=1/2.75, max_depth=1/0.009),
-        MaskDepthByMinMax(min_depth=1/2.75, max_depth=1/0.009),
+        Scale3DEqualizedBatch(p=1, min_depth=1 / 2.75, max_depth=1 / 0.009),
+        MaskDepthByMinMax(min_depth=1 / 2.75, max_depth=1 / 0.009),
+    ]
+    transform = torchvision.transforms.Compose(transforms)
+    return transform
+
+
+@register_batch_augmentation
+def mvsnet_batch_augmentations(**kwargs):
+    transforms = [
+        Scale3DEqualizedBatch(p=1, min_depth=1 / 2.75, max_depth=1 / 0.009),
+        MaskDepthByMinMax(min_depth=1 / 2.75, max_depth=1 / 0.009),
     ]
     transform = torchvision.transforms.Compose(transforms)
     return transform
