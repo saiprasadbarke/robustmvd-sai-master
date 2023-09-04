69c80549234a213b763bbefe4f0beb55a3521dc5
saiprasadbarke <barke.saiprasad1992@gmail.com>
2023-09-04 12:49:40 +0200

    modified dispnetcontext encoder

--- rmvd/models/blocks/dispnet_context_encoder.py
+++ rmvd/models/blocks/dispnet_context_encoder.py
@@ -4,9 +4,9 @@ from .utils import conv
 
 
 class DispnetContextEncoder(nn.Module):
-    def __init__(self):
+    def __init__(self, in_channels=256):
         super().__init__()
-        self.conv_redir = conv(256, 32, kernel_size=1, stride=1)
+        self.conv_redir = conv(in_channels, 32, kernel_size=1, stride=1)
 
     def forward(self, conv3):
         conv_redir = self.conv_redir(conv3)
